89cfdc68bfa05a58451b3aea7d38684e6adcd9b4
raisakbar27 <raisakbarwibowo27@gmail.com>
2025-05-27 22:58:45 +1000

    connect to mongo atlas

--- api/index.js
+++ api/index.js
@@ -1,8 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(err => {
+    console.error('Error connecting to MongoDB:', err);
+});
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000???');
-    }
-);
+    console.log('Server is running on http://localhost:3000');
+});
