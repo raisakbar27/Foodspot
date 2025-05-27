1812d356908ddc404580cb4511ef0fe5236e920c
raisakbar27 <raisakbarwibowo27@gmail.com>
2025-05-28 00:12:54 +1000

    create api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routers/user.router.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGODB).then(() => {
@@ -14,3 +15,9 @@ const app = express();
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
 });
+
+app.get('/test', (req, res) => {
+    res.send('Welcome to the API');
+});
+
+app.use("/api/user", userRouter);
