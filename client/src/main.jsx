65da4dc4c1bfd0de2ab06c04f422e5570d2d8606
raisakbar27 <raisakbarwibowo27@gmail.com>
2025-06-08 13:38:27 +1000

    add redux

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,16 @@
-import React, { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { persistor,store } from "./redux/store.js";
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
 );
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,47 +1,44 @@
-import React , {useState} from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function Login() {
-
-  const [formData, setFormData] = useState({})
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [formData, setFormData] = useState({});
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     setFormData({
       ...formData,
-      [e.target.id]:
-        e.target.value,
+      [e.target.id]: e.target.value,
     });
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
-      const res = await fetch('/api/auth/login', 
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(formData),
-        }
-      );
+      dispatch(signInStart());
+      const res = await fetch("/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
       const data = await res.json();
       if (data.success === false) {
-        setLoading(false);
-        setError(data.message);
-        
+        dispatch(signInFailure(data.message));
         return;
       }
-      setLoading(false);
-      setError(null);
-      console.log(data);
-      navigate('/');
+      dispatch(signInSuccess(data));
+      navigate("/");
     } catch (error) {
-      setLoading(false);
-      setError("setError: " + error.message);
+      dispatch(signInFailure(error.message));
     }
   };
 
@@ -81,7 +78,7 @@ export default function Login() {
           {loading ? "Creating Account..." : "Log In"}
         </button>
       </form>
-      <div className='flex gap-2 mt-5'>
+      <div className="flex gap-2 mt-5">
         <p>Don't have an account?</p>
         <Link to={"/signup"}>
           <span className="text-slate-500">Sign Up</span>
